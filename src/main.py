472fc32be2fff4b42abc81cd1e9c1f04ae1db076
mahathirmoon <maahthitmoon1@email.com>
2026-05-01 23:36:51 +0600

    added print functionalities in main.py

--- src/main.py
+++ src/main.py
@@ -6,3 +6,18 @@ print("1st May 2026")
 utils.add(2,3)
 utils.subtract(5,1)
 utils.mul(2,3)
+
+j= 3
+n=4
+i=0
+
+while(i<4):
+    while(j<4):
+           print("*",end="")
+           j+=1
+    #print("\n")
+    j-=1
+    i+=1
+
+
+
