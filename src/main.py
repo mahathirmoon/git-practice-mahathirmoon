34d852e39c0419a3766da58e58082b3c2b447663
mahathirmoon <maahthitmoon1@email.com>
2026-05-01 22:52:19 +0600

    added basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
+import utils
 print("Mahathir Moon")
 print("1st May 2026")
+
+
+utils.add(2,3)
+utils.subtract(5,1)
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+def add (a,b):
+    print(a+b)
+
+
+
+def subtract (a,b):
+    print(a-b)
